48a35ecacad8c0581b12f12824cecbb070e8adc0
jas6zhang <jas6zhang@gmail.com>
2021-02-21 16:56:13 -0500

    animation

--- scrolling.js
+++ scrolling.js
@@ -1,32 +1,47 @@
 window.addEventListener('scroll', reveal);
 
-    function reveal(){
-      var reveals = document.querySelectorAll('.reveal');
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
 
-      for(var i = 0; i < reveals.length; i++){
+  for (var i = 0; i < reveals.length; i++) {
 
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
 
-        if(revealtop < windowheight - revealpoint){
-          reveals[i].classList.add('active');
-        }
-      }
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
     }
+  }
+}
 
 window.addEventListener('scroll', revealimg);
-    function revealimg(){
-      var reveals = document.querySelectorAll('.reveal-img');
 
-      for(var i = 0; i < reveals.length; i++){
+function revealimg() {
+  var reveals = document.querySelectorAll('.reveal-img');
 
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
+  for (var i = 0; i < reveals.length; i++) {
 
-        if(revealtop < windowheight - revealpoint){
-          reveals[i].classList.add('active');
-        }
-      }
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
     }
+  }
+}
+
+var i = 0;
+var txt = '- Lifelong Student & Occasional Programmer.'; /* The text */
+var speed = 80; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter()
